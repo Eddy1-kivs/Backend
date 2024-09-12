04cb734c03bfec6263a682b82c020d98f5c06f8b
Eddy1-kivs <kivseddy@gmail.com>
2024-09-12 13:25:22 +0300

    hello

--- Backend/settings.py
+++ Backend/settings.py
@@ -252,7 +252,8 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (uploads)
 MEDIA_URL = '/media/'
